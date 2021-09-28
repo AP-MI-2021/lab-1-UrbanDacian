586cf910377ad9cac0a1c9f444e7603c33e17482
UrbanDacian <dacianurban13@gmail.com>
2021-09-28 14:10:06 +0300

    Functia este_prim

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-'''
-Returneaza true daca n este prim si false daca nu.
-'''
-def is_prime(n):
-  # codul vostru aici
-  
-  
-'''
-Returneaza produsul numerelor din lista lst.
-'''
-def get_product(lst):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind primul algoritm.
-'''
-def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
-'''
-def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
-def main():
-  # interfata de tip consola aici
-
-if __name__ == '__main__':
-  main()
+x = int(input())
+pr=1
+for i in range ( 2 , int(x/2)+1):
+    if x%i ==0:
+        pr=0
+if pr ==0:
+    print ("Nu este Prim")
+else :
+    print ("Prim")
